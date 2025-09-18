88eb7ddf05af9467e27cf9ea2e37ec81f86a5379
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-18 13:05:48 -0500

    seccion de footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Hero } from "./sections/Hero";
 import { Features } from "./sections/Features";
 import { Testimonials } from "./sections/Testimonials";
 import { Contact } from "./sections/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Features></Features>
       <Testimonials></Testimonials>
       <Contact></Contact>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className="footer__container">
+        <p className="footer__parrafo">
+          © 2024 Jhon's. Todos los derechos reservados.
+        </p>
+      </footer>
+    </>
+  );
+};
